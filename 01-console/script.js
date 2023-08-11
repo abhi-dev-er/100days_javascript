4d6e6fe858909445a182a9ac0e7860024c3ed933
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2023-08-11 10:15:26 -0700

    my first commit

---
+++ 01-console/script.js
@@ -0,0 +1,29 @@
+console.log(100);
+
+console.log('hello World!');
+
+console.log(14, 'hello', false);
+
+
+const num = 100;
+
+console.log(num);
+
+console.error('hello')
+
+console.warn('hello');
+
+console.table({name: 'john', email: 'xyz@gmail.com', age: 23});
+
+
+console.group('simple');
+
+console.log(num);
+console.error('hello')
+console.warn('hello');
+
+console.groupEnd();
+
+const styles ='padding: 10px; background-color: black; color: white;'
+
+console.log('%chello Abhishek', styles);
