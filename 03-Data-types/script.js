123fcc8be15fdfd18a2ca66d8a8ec525c6e35f22
abhishek <116533586+abhi-dev-er@users.noreply.github.com>
2023-08-12 09:17:25 -0700

    day 3 data types in javascript

---
+++ 03-Data-types/script.js
@@ -0,0 +1,37 @@
+// Primitive Data Types
+
+// Strings, Numbers, Boolean, Null , Undefined, Symbol, BigInt
+
+let x;
+
+x = 'Hello World'
+console.log(x, typeof x);
+
+//String
+
+x = 3.96
+console.log(x, typeof x);
+//number
+
+x = true
+console.log(x, typeof x);
+//boolean
+
+x = null
+console.log(x, typeof x);
+
+//object
+
+let firstname;
+console.log( firstname, typeof firstname);
+// undefined
+
+
+
+x =Symbol('hello');
+console.log(x, typeof x);
+
+//symbol
+
+x =32523423226n;
+console.log(x, typeof x);
