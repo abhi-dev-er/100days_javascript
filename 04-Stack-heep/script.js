40e2ab0a72bad88db9e3e71a2aa960f7f48448ec
abhishek <116533586+abhi-dev-er@users.noreply.github.com>
2023-08-13 11:40:44 -0700

    Stack-heep

---
+++ 04-Stack-heep/script.js
@@ -0,0 +1,19 @@
+// Values are stored in stack
+
+let name = 'tom';
+const age = 23;
+
+let newName = 'john'
+console.log(name, newName);
+
+
+// Reference Values are stored in heep
+
+const student ={
+    name: 'tom',
+    age: 23
+}
+
+let studentdata = student;
+// studentdata.name = 'john'
+console.log(student, studentdata);
