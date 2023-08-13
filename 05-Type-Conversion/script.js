238c42af86fd814cf6d5efc3c8cc84c634499e48
abhishek <116533586+abhi-dev-er@users.noreply.github.com>
2023-08-13 11:41:55 -0700

    Type-Conversion

---
+++ 05-Type-Conversion/script.js
@@ -0,0 +1,38 @@
+let x;
+
+x = '23';
+
+//convert string to number
+
+x =  parseInt(x);
+x = +x;
+x = Number(x);
+
+
+
+// convert number to string
+
+x = 100;
+
+x = x.toString();
+x = String(x);
+
+
+
+
+// convert  string to decimal 
+
+x = '3.43'
+x = parseFloat(x);
+
+
+// convert number to Boolean
+
+x = 1;
+
+x = Boolean(x)
+
+
+console.log(x, typeof x);
+
+
