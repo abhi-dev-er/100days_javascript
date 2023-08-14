dab6ec574b66079f8ecce14135ec2d292c2015a6
abhishek <116533586+abhi-dev-er@users.noreply.github.com>
2023-08-14 03:35:48 -0700

    Operators

---
+++ 06-Operators/script.js
@@ -0,0 +1,70 @@
+// Arithmetic Operators 
+
+let x;
+
+x = 3 + 5;  
+// 8
+x = 9 - 5;
+// 4
+x = 12 * 5;
+// 60
+x = 15 / 5;
+// 3
+x = 8 % 5;
+// 3
+x = 2 ** 3
+//8
+
+
+// Assignment Operators
+
+x =10;
+x += 4;
+// 14
+x -= 4;
+// 10
+x *= 4;
+//40
+x /= 4;
+//10
+x %= 4;
+//2
+x **= 4
+//16
+
+
+
+// Comparison Operators
+
+x = 10 == 10;
+//true
+
+x = 10 ==2;
+//false
+
+x = 2 === 2;
+//true 
+
+x= 2==='2';
+//false
+
+x = 2 != 2
+//false
+
+x = 2 !== '2';
+//true
+
+x = 6 > 5;
+// true
+
+x = 6 < 5;
+//false
+
+x = 5 <= 5;
+//true
+
+x = 6 >= 5;
+//true
+
+
+console.log(x);
