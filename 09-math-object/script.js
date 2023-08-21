01ab465dad3fed3e2c6cd4bfa512def3cfa7e9a9
abhishek <116533586+abhi-dev-er@users.noreply.github.com>
2023-08-21 08:28:03 -0700

    math-object

---
+++ 09-math-object/script.js
@@ -0,0 +1,63 @@
+// console.log(Math);
+
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x= Math.round(7.66);
+
+x =Math.ceil(4.4);
+
+x = Math.floor(4.4);
+
+x =Math.pow(2,4);
+
+x = Math.max(4, 7);
+
+x = Math.min(3,4);
+
+x = Math.random();
+
+x = Math.random()*10;
+
+x = Math.random()*10 +1;
+
+x =Math.floor(Math.random()*10 +1);
+
+x =Math.floor(Math.random()*100 +1);
+
+
+// console.log(x);
+
+
+const num1 = Math.floor(Math.random()*100 +1);
+const num2 = Math.floor( Math.random()*50 +1);
+
+
+const sum = num1 + num2
+const sumOutput = `${num1} + ${num2}  = ${sum}`
+console.log(sumOutput);
+
+
+const sum2 = num1 - num2
+const sumOutput2 = `${num1} - ${num2}  = ${sum2}`
+console.log(sumOutput2);
+
+
+
+const sum3 = num1 * num2
+const sumOutput3 = `${num1} * ${num2} = ${sum3}`
+console.log(sumOutput3);
+
+
+
+const sum4 = num1 / num2
+const sumOutput4 = `${num1} / ${num2} = ${sum4}`
+console.log(sumOutput4);
+
+
+const sum5 = num1 % num2
+const sumOutput5 = `${num1} % ${num2} = ${sum5}`
+console.log(sumOutput5);
